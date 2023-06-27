e9c1a94263452fb651bec02e81e13d79cc5cbacd
baotuyloan1 <nguyenducbaokey@gmail.com>
2023-06-27 16:56:23 +0700

    Datatype

---
+++ DataType/SQLQuery2.sql
@@ -0,0 +1,13 @@
+﻿-- Các kiểu dữ liệu hay dùng
+
+--int: kiểu số nguyên, tập hợp N. Ví dụ: -9, 8, -6 , 3, 10
+--float: kiểu số thực, tập hợp R. Ví dụ: -0.5, -0.9 , 10, 9.6
+--char: kiểu kí tự. không viết tiếng việt được. Bộ nhớ cấp phát tĩnh. Ví dụ: A, a, B, b, f,k, K. char(10) -> 10 ô nhớ này không ai được lấy. 'Bao' luôn dữ 10 ô nhớ
+--varchar: kiểu kí tự. không viết tiếng việt được Bộ nhớ cấp phát động. varchar(10)-> 10 ô nhớ này chỉ được lấy khi có dữ liệu nằm bên trong. 'Bao' -> chỉ dùng 3 ô nhớ
+--nchar: kiểu kí tự cấp phát tĩnh. có thể lưu tiếng việc được, y chan char
+--nvarchar: kiểu kí tự cấp phát động có thể lưu tiếng việt
+--date: lưu trữ ngày, tháng, năm, giờ
+--time: lưu trữ giờ, phút, giây...
+--byte: lưu giá trị 0 và 1
+--text: lưu văn bản lớn
+--ntext: lưu văn bản lớn có tiếng việt (Unicode)
